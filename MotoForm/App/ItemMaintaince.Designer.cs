f2eaa811d823d0ce43750aab6692879fba1b47c4
Carter <zongu3317@gmail.com>
2019-04-04 15:40:14 +0800

    [Updated] 加入忽略列表

--- MotoForm/App/ItemMaintaince.Designer.cs
+++ MotoForm/App/ItemMaintaince.Designer.cs
@@ -32,25 +32,25 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.btnRepairCategoryAll = new System.Windows.Forms.Button();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lbItemTotalCount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbCategoryDisplayName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.splitContainer3 = new System.Windows.Forms.SplitContainer();
             this.gvRepairItem = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lbCategoryDisplayName = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.lbItemTotalCount = new System.Windows.Forms.Label();
-            this.btnAddRepairItem = new System.Windows.Forms.Button();
-            this.btnEditRepairItem = new System.Windows.Forms.Button();
-            this.btnDisableRepairItem = new System.Windows.Forms.Button();
-            this.repairItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.repairItemBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.repairItemBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
-            this.customRepairItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.RepairItemId = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ItemName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CategoryDisplayName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customRepairItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnDisableRepairItem = new System.Windows.Forms.Button();
+            this.btnEditRepairItem = new System.Windows.Forms.Button();
+            this.btnAddRepairItem = new System.Windows.Forms.Button();
+            this.repairItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.repairItemBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.repairItemBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -64,10 +64,10 @@
             this.splitContainer3.Panel2.SuspendLayout();
             this.splitContainer3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gvRepairItem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customRepairItemBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.repairItemBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.repairItemBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.repairItemBindingSource2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.customRepairItemBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -120,6 +120,53 @@
             this.splitContainer2.SplitterDistance = 53;
             this.splitContainer2.TabIndex = 0;
             // 
+            // lbItemTotalCount
+            // 
+            this.lbItemTotalCount.AutoSize = true;
+            this.lbItemTotalCount.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbItemTotalCount.Location = new System.Drawing.Point(390, 15);
+            this.lbItemTotalCount.Name = "lbItemTotalCount";
+            this.lbItemTotalCount.Size = new System.Drawing.Size(43, 21);
+            this.lbItemTotalCount.TabIndex = 4;
+            this.lbItemTotalCount.Text = "999";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(467, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "項";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "，共有";
+            // 
+            // lbCategoryDisplayName
+            // 
+            this.lbCategoryDisplayName.AutoSize = true;
+            this.lbCategoryDisplayName.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbCategoryDisplayName.Location = new System.Drawing.Point(200, 15);
+            this.lbCategoryDisplayName.Name = "lbCategoryDisplayName";
+            this.lbCategoryDisplayName.Size = new System.Drawing.Size(54, 21);
+            this.lbCategoryDisplayName.TabIndex = 1;
+            this.lbCategoryDisplayName.Text = "全部";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "目前顯示類別：";
+            // 
             // splitContainer3
             // 
             this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -158,67 +205,65 @@
             this.gvRepairItem.Location = new System.Drawing.Point(0, 0);
             this.gvRepairItem.MultiSelect = false;
             this.gvRepairItem.Name = "gvRepairItem";
+            this.gvRepairItem.ReadOnly = true;
             this.gvRepairItem.RowTemplate.Height = 24;
             this.gvRepairItem.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.gvRepairItem.Size = new System.Drawing.Size(840, 596);
             this.gvRepairItem.TabIndex = 0;
             // 
-            // label1
+            // RepairItemId
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 21);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "目前顯示類別：";
+            this.RepairItemId.DataPropertyName = "RepairItemId";
+            this.RepairItemId.HeaderText = "RepairItemId";
+            this.RepairItemId.Name = "RepairItemId";
+            this.RepairItemId.ReadOnly = true;
+            this.RepairItemId.Visible = false;
             // 
-            // lbCategoryDisplayName
+            // ItemName
             // 
-            this.lbCategoryDisplayName.AutoSize = true;
-            this.lbCategoryDisplayName.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.lbCategoryDisplayName.Location = new System.Drawing.Point(200, 15);
-            this.lbCategoryDisplayName.Name = "lbCategoryDisplayName";
-            this.lbCategoryDisplayName.Size = new System.Drawing.Size(54, 21);
-            this.lbCategoryDisplayName.TabIndex = 1;
-            this.lbCategoryDisplayName.Text = "全部";
+            this.ItemName.DataPropertyName = "ItemName";
+            this.ItemName.HeaderText = "項目名稱";
+            this.ItemName.Name = "ItemName";
+            this.ItemName.ReadOnly = true;
+            this.ItemName.Width = 500;
             // 
-            // label2
+            // Category
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(295, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 21);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "，共有";
+            this.Category.DataPropertyName = "Category";
+            this.Category.HeaderText = "Category";
+            this.Category.Name = "Category";
+            this.Category.ReadOnly = true;
+            this.Category.Visible = false;
             // 
-            // label3
+            // CategoryDisplayName
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(467, 15);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 21);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "項";
+            this.CategoryDisplayName.DataPropertyName = "CategoryDisplayName";
+            this.CategoryDisplayName.HeaderText = "類別";
+            this.CategoryDisplayName.Name = "CategoryDisplayName";
+            this.CategoryDisplayName.ReadOnly = true;
+            this.CategoryDisplayName.Width = 150;
             // 
-            // lbItemTotalCount
+            // Price
             // 
-            this.lbItemTotalCount.AutoSize = true;
-            this.lbItemTotalCount.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.lbItemTotalCount.Location = new System.Drawing.Point(390, 15);
-            this.lbItemTotalCount.Name = "lbItemTotalCount";
-            this.lbItemTotalCount.Size = new System.Drawing.Size(43, 21);
-            this.lbItemTotalCount.TabIndex = 4;
-            this.lbItemTotalCount.Text = "999";
+            this.Price.DataPropertyName = "Price";
+            this.Price.HeaderText = "價錢";
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            this.Price.Width = 150;
             // 
-            // btnAddRepairItem
+            // customRepairItemBindingSource
             // 
-            this.btnAddRepairItem.Location = new System.Drawing.Point(20, 25);
-            this.btnAddRepairItem.Name = "btnAddRepairItem";
-            this.btnAddRepairItem.Size = new System.Drawing.Size(280, 70);
-            this.btnAddRepairItem.TabIndex = 0;
-            this.btnAddRepairItem.Text = "新增維修項目";
-            this.btnAddRepairItem.UseVisualStyleBackColor = true;
-            this.btnAddRepairItem.Click += new System.EventHandler(this.RepairItemOperation);
+            this.customRepairItemBindingSource.DataSource = typeof(MotoForm.Model.CustomRepairItem);
+            // 
+            // btnDisableRepairItem
+            // 
+            this.btnDisableRepairItem.Location = new System.Drawing.Point(20, 125);
+            this.btnDisableRepairItem.Name = "btnDisableRepairItem";
+            this.btnDisableRepairItem.Size = new System.Drawing.Size(280, 70);
+            this.btnDisableRepairItem.TabIndex = 2;
+            this.btnDisableRepairItem.Text = "刪除維修項目";
+            this.btnDisableRepairItem.UseVisualStyleBackColor = true;
+            this.btnDisableRepairItem.Click += new System.EventHandler(this.RepairItemOperation);
             // 
             // btnEditRepairItem
             // 
@@ -230,15 +275,15 @@
             this.btnEditRepairItem.UseVisualStyleBackColor = true;
             this.btnEditRepairItem.Click += new System.EventHandler(this.RepairItemOperation);
             // 
-            // btnDisableRepairItem
+            // btnAddRepairItem
             // 
-            this.btnDisableRepairItem.Location = new System.Drawing.Point(20, 125);
-            this.btnDisableRepairItem.Name = "btnDisableRepairItem";
-            this.btnDisableRepairItem.Size = new System.Drawing.Size(280, 70);
-            this.btnDisableRepairItem.TabIndex = 2;
-            this.btnDisableRepairItem.Text = "刪除維修項目";
-            this.btnDisableRepairItem.UseVisualStyleBackColor = true;
-            this.btnDisableRepairItem.Click += new System.EventHandler(this.RepairItemOperation);
+            this.btnAddRepairItem.Location = new System.Drawing.Point(20, 25);
+            this.btnAddRepairItem.Name = "btnAddRepairItem";
+            this.btnAddRepairItem.Size = new System.Drawing.Size(280, 70);
+            this.btnAddRepairItem.TabIndex = 0;
+            this.btnAddRepairItem.Text = "新增維修項目";
+            this.btnAddRepairItem.UseVisualStyleBackColor = true;
+            this.btnAddRepairItem.Click += new System.EventHandler(this.RepairItemOperation);
             // 
             // repairItemBindingSource
             // 
@@ -251,45 +296,6 @@
             // repairItemBindingSource2
             // 
             this.repairItemBindingSource2.DataSource = typeof(MotoForm.Domain.Model.RepairItem);
-            // 
-            // customRepairItemBindingSource
-            // 
-            this.customRepairItemBindingSource.DataSource = typeof(MotoForm.Model.CustomRepairItem);
-            // 
-            // RepairItemId
-            // 
-            this.RepairItemId.DataPropertyName = "RepairItemId";
-            this.RepairItemId.HeaderText = "RepairItemId";
-            this.RepairItemId.Name = "RepairItemId";
-            this.RepairItemId.Visible = false;
-            // 
-            // ItemName
-            // 
-            this.ItemName.DataPropertyName = "ItemName";
-            this.ItemName.HeaderText = "項目名稱";
-            this.ItemName.Name = "ItemName";
-            this.ItemName.Width = 500;
-            // 
-            // Category
-            // 
-            this.Category.DataPropertyName = "Category";
-            this.Category.HeaderText = "Category";
-            this.Category.Name = "Category";
-            this.Category.Visible = false;
-            // 
-            // CategoryDisplayName
-            // 
-            this.CategoryDisplayName.DataPropertyName = "CategoryDisplayName";
-            this.CategoryDisplayName.HeaderText = "類別";
-            this.CategoryDisplayName.Name = "CategoryDisplayName";
-            this.CategoryDisplayName.Width = 150;
-            // 
-            // Price
-            // 
-            this.Price.DataPropertyName = "Price";
-            this.Price.HeaderText = "價錢";
-            this.Price.Name = "Price";
-            this.Price.Width = 150;
             // 
             // ItemMaintaince
             // 
@@ -316,10 +322,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
             this.splitContainer3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gvRepairItem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customRepairItemBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.repairItemBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.repairItemBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.repairItemBindingSource2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.customRepairItemBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
